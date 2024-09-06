7fc2de840984fb8b851dc4efb25a0d58b2592589
Sourav Chakraborty <123477647+souravvvv123@users.noreply.github.com>
2024-09-06 15:45:25 +0530

    Update longest_common_prefix.py

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,1 +1,16 @@
+strs = ["flower", "flow", "floght"]
 
+
+if not strs:
+    print("empty")
+    
+    
+prefix = strs[0]
+
+
+for s in strs[1:]: 
+    while not s.startswith(prefix):
+        prefix = prefix[:-1]
+        
+print(prefix)
+        
